670b9c4cc369279075708b56e54e94673946e73c
venkatesh seepana <venkeyvenkatroy@gmail.com>
2022-10-12 14:42:22 +0530

    Commit Done On 12-10-2022,14:42 PM

--- MyProjectBFS.py
+++ MyProjectBFS.py
@@ -8,6 +8,10 @@ def sample():
 @app.route("/signup")
 def sample2():
     return render_template('MyProjectSignup.html')
+@app.route("/login")
+def sample3():
+    return render_template('MyProjectLogin.html')
+
 #main method
 if __name__=="__main__":
     app.run()
